14b2a659c841b934ee5227fb041b2f36c63a1f4e
Bthouverez <you@example.com>
2017-01-13 17:02:13 +0100

    clean

--- Personnage/obj/x64/debug/BVHChannel.d
+++
@@ -1,6 +0,0 @@
-obj/x64/debug/BVHChannel.o: src/master_CharAnim/BVHChannel.cpp \
- src/master_CharAnim/BVHChannel.h src/master_CharAnim/BVHAxis.h
-
-src/master_CharAnim/BVHChannel.h:
-
-src/master_CharAnim/BVHAxis.h:
